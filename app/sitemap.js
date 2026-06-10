42597ac65f791f47361132b5dfa5677cf1773c3e
colin-mtd <cjcoles@virginmedia.com>
2026-06-10 21:52:36 +0100

    Create sitemap.js
    Created a site map

---
+++ app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://mtd-for-landlords.co.uk",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mtd-for-landlords.co.uk/agents",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mtd-for-landlords.co.uk/privacy-policy",
+      lastModified: new Date(),
+    },
+  ];
+}
