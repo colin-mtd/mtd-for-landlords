ad758d78eac73d6569db07af42ab25db53e20fdd
colin-mtd <cjcoles@virginmedia.com>
2026-06-29 13:39:59 +0100

    Update sitemap.js
    Updated sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,16 +1,42 @@
 export default function sitemap() {
+  const lastModified = new Date();
+
   return [
     {
       url: "https://mtd-for-landlords.co.uk",
-      lastModified: new Date(),
+      lastModified,
     },
     {
       url: "https://mtd-for-landlords.co.uk/agents",
-      lastModified: new Date(),
+      lastModified,
     },
     {
       url: "https://mtd-for-landlords.co.uk/privacy-policy",
-      lastModified: new Date(),
+      lastModified,
+    },
+    {
+      url: "https://mtd-for-landlords.co.uk/guides",
+      lastModified,
+    },
+    {
+      url: "https://mtd-for-landlords.co.uk/guides/what-is-making-tax-digital-for-landlords",
+      lastModified,
+    },
+    {
+      url: "https://mtd-for-landlords.co.uk/guides/mtd-threshold-for-landlords",
+      lastModified,
+    },
+    {
+      url: "https://mtd-for-landlords.co.uk/guides/jointly-owned-property-and-mtd",
+      lastModified,
+    },
+    {
+      url: "https://mtd-for-landlords.co.uk/guides/mtd-faq-for-landlords",
+      lastModified,
+    },
+    {
+      url: "https://mtd-for-landlords.co.uk/guides/landlord-compliance-checklist",
+      lastModified,
     },
   ];
 }
